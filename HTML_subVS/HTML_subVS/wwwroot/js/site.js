0901ecd9b43f5bb11d616cfcab58a8fb242ef966
joelmindset <82834522+joelmindset@users.noreply.github.com>
2021-07-27 12:35:30 -0600

    VS sub assignment
    set up a basic html in VS
    add a youtube video
    create some js

---
+++ HTML_subVS/HTML_subVS/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+// animation for moving dot across container from top left to the bottom right
+var id = null;
+function moveDot() {
+    var elem = document.getElementById("animation1");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}

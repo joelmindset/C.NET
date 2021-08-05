b55fdf153258cdcd54d3023bf206764bb2d27fac
joelmindset <82834522+joelmindset@users.noreply.github.com>
2021-08-05 10:56:21 -0600

    Branching
    packing estimate cost for customer

---
+++ Branching_sub/Branching_sub/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace Branching_sub
+{
+    class Program
+    {
+        // program for shipping cost for costumer 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight:");
+            // Objects for box and shipping costs
+            double weight = Convert.ToDouble(Console.ReadLine());
+            double width = 0;
+            double height = 0;
+            double length = 0;
+            double dimension = 0;
+            decimal estimate = 0.00m;
+            // under 50 weight get dimension / over weight exit
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            } else
+            {
+                Console.WriteLine("Please enter the package width:");
+                width = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Please enter the package height:");
+                height = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Please enter the package length:");
+                length = Convert.ToDouble(Console.ReadLine());
+                dimension = length * height * width;
+
+
+            }
+            // under 50 dimensions get estimate and present to user / over dimensions  exit
+            if (dimension < 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+            } else
+            {
+                estimate = Convert.ToDecimal(dimension * weight / 100.00) + 0.00m;
+                Console.WriteLine("Your estimated total for shipping this package is: $" + estimate);
+                Console.WriteLine("Thank you!");
+                Console.Read();
+            }
+        }
+    }
+}

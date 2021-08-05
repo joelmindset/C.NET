888911c107eb4eafbf7b9a7c7d9aced2e93e3025
joelmindset <82834522+joelmindset@users.noreply.github.com>
2021-08-05 14:03:38 -0600

    do and while loops

---
+++ ConsoleAppSub/ConsoleAppSub/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a color?");
+            string color = Console.ReadLine();
+            bool guessColor = color == "green";
+
+            while (!guessColor)
+            {
+                switch(color)
+                {
+                    case "green":
+                        Console.WriteLine("You guess right!");
+                        
+                        break;
+                    default:
+                        Console.WriteLine("Guess again.....");
+                        color = Console.ReadLine();
+                        break;
+                }
+            }
+            Console.WriteLine("Lets guess another color.");
+            string color2 = Console.ReadLine();
+            do
+                switch (color2)
+                {
+                    case "blue":
+                        Console.WriteLine("You guess right! You've wont the game.");
+                        guessColor = true;
+                        
+                        break;
+                    default:
+                        Console.WriteLine("Guess again.....");
+                        color2 = Console.ReadLine();
+                        break;
+                }
+            while (!guessColor);
+            Console.ReadLine();
+        }
+        
+    }
+}

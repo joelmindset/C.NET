043e4a7bfb95f43153bfb97c17a7fe1fb4b3a107
joelmindset <82834522+joelmindset@users.noreply.github.com>
2021-08-11 12:58:53 -0600

    Methods
    simple calculator

---
+++ Methods_sub/Methods_sub/MathOps.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_sub
+{
+    public class MathOps
+    {
+        // data variables 
+        public int X { get; set; }
+        public int A { get; set; }
+        public string Operator { get; set; }
+      
+        public MathOps()
+        {
+
+            // assign 
+            X = 0;
+            A = 1;
+            Operator = "+";
+            
+        }
+        // simple calculator for GetAnswer function
+        public void GetAnswer()
+        {   
+            // variable objects for switch
+            switch (Operator)
+            {
+                case "+":
+                    int Answer = X + A;
+                    Console.WriteLine(Answer);
+                    break;
+                case "-":
+                    Answer = X - A;
+                    Console.WriteLine(Answer);
+                    break;
+                case "/":
+                    Answer = X / A;
+                    Console.WriteLine(Answer);
+                    break;
+                case "*":
+                    Answer = X * A;
+                    Console.WriteLine(Answer);
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator");
+                    break;
+            }
+            
+        }
+
+    }
+}
---
+++ Methods_sub/Methods_sub/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_sub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // mathOps call
+            MathOps mathOps = new MathOps();
+            // user input number in mathOps.A
+            Console.WriteLine("What is the first number?");
+            mathOps.A = Convert.ToInt32(Console.ReadLine());
+            // user input number in mathOps.Operator
+            Console.WriteLine("What operator would you like to use?");
+            mathOps.Operator = Convert.ToString(Console.ReadLine());
+            // user input number in mathOps.X
+            Console.WriteLine("What is the second number?");
+            mathOps.X = Convert.ToInt32(Console.ReadLine());
+            
+            // call GetAnswer function
+            mathOps.GetAnswer();
+            Console.ReadLine();
+            
+        }
+    }
+}

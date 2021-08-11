8e9a9b18b4eabe430277539d9495bcb31fe139d4
joelmindset <82834522+joelmindset@users.noreply.github.com>
2021-08-11 13:50:10 -0600

    call methods
    better calculator

---
+++ CallMethods/CallMethods/MathOperation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallMethods
+{
+    public class MathOperation
+    {
+        // times perimeters 
+        public int getMulti(int X, int A)
+        {
+            return X * A;
+        }
+        // divide perimeters
+        public int getDiv(int X, int A)
+        {
+            return X / A;
+        }
+        // add perimeters
+        public int getAdd(int X, int A)
+        {
+            return X + A;
+        }
+        // minus perimeters
+        public int getMinus(int X, int A)
+        {
+            return X - A;
+        }
+    }
+}
---
+++ CallMethods/CallMethods/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // mathOps call
+           
+            MathOperation mathOp = new MathOperation();
+            // user input number in mathOps.A
+            Console.WriteLine("What is the first number?");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            // user input number in mathOps.Operator
+            Console.WriteLine("What operator would you like to use?");
+            string Operator = Convert.ToString(Console.ReadLine());
+            // user input number in mathOps.X
+            Console.WriteLine("What is the second number?");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            
+            // switch for Operator to call methods 
+            switch (Operator)
+            {
+                case "+":
+                    Console.WriteLine(mathOp.getAdd(num1, num2));
+                    break;
+                case "-":
+                    Console.WriteLine(mathOp.getMinus(num1, num2));
+                    break;
+                case "/":
+                    Console.WriteLine(mathOp.getDiv(num1, num2));
+                    break;
+                case "*":
+                    Console.WriteLine(mathOp.getMulti(num1, num2));
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}

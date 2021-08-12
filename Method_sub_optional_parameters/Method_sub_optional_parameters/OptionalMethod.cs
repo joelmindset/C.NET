27e92ef622548b57390fc79ff849da8a1a3bc863
joelmindset <82834522+joelmindset@users.noreply.github.com>
2021-08-12 10:06:37 -0600

    methods
    optional parameters

---
+++ Method_sub_optional_parameters/Method_sub_optional_parameters/OptionalMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_sub_optional_parameters
+{
+    class OptionalMethod
+    {
+        public int MathInt(int math1, int math2 = 2)
+        {
+            // times numbers
+            return math1 * math2;
+        }
+    }
+}
---
+++ Method_sub_optional_parameters/Method_sub_optional_parameters/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_sub_optional_parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // call for class
+            OptionalMethod opMath = new OptionalMethod();
+            Console.WriteLine("Please enter a number: ");
+            // input from user
+            int userInt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a number. This one is optional nothing is needed: ");
+            // input from user            
+            // try if user puts value for Int
+            try
+            {
+                Console.WriteLine("lets multiply!");
+                int userInt2 = Convert.ToInt32(Console.ReadLine());
+                // call OptionalMethod class mathInt method
+                Console.WriteLine(opMath.MathInt(userInt, userInt2));
+            }
+            // if user only puts first value in userInt
+            catch (FormatException)
+            {
+                Console.WriteLine("Your second number was left blank or was not a number. Lets fill it in with a number 2 and multiply!");
+                Console.WriteLine(opMath.MathInt(userInt));
+            }
+            Console.ReadLine();
+        }
+    }
+}

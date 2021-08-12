4a3ba445418aa9cf3c442e8a43b7a785755ef359
joelmindset <82834522+joelmindset@users.noreply.github.com>
2021-08-12 09:29:35 -0600

    Methods
    Methods
    Data types
    Converting
    try and catch block

---
+++ MainMethodSub/MainMethodSub/MathMethod.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSub
+{
+    public class MathMethod
+    {
+        
+        // function for Integer | user input for math1.
+        // math2 default = int 2
+        public int MathInt(int math1, int math2 = 2)
+        {
+            // times numbers
+            return math1 * math2;
+        }
+        // function for decimal | user input for mathDec1
+        // mathDec2 default = decimal 2
+        public int MathDecimal(decimal mathDec1, decimal mathDec2 = 2)
+        {
+            // times decimals then convert to int store in answer
+            int answer = Convert.ToInt32(mathDec1 * mathDec2);
+            //return answer to user
+            return answer;
+        }
+        // function for string | user input for mathString
+        // math2 default = int 2
+        public int MathString(string mathString, int math2 = 2)
+        {
+            // try catch for int and string converter
+            try
+            {
+                // convert string to int 
+                int stringConvertInt = Convert.ToInt32(mathString);
+                // times our numbers store in answer
+                int answer = stringConvertInt * math2;
+                // return answer to user
+                return answer;
+            }
+            catch (Exception err)
+            {
+                int answer = 0;
+                // show error to user
+                Console.WriteLine(err.Message);
+                return answer;
+            }
+        }
+    }
+}
---
+++ MainMethodSub/MainMethodSub/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // call for class
+            MathMethod getMath = new MathMethod();
+            Console.WriteLine("Please enter a number: ");
+            // input from user
+            int userInt = Convert.ToInt32(Console.ReadLine());
+            // call MathInt method
+            Console.WriteLine(getMath.MathInt(userInt));
+            Console.WriteLine("Please enter a number or decimal. Number will be rounded: ");
+            // input from user
+            decimal userDec = Convert.ToDecimal(Console.ReadLine());
+            // call MathDecimal method returns int
+            Console.WriteLine(getMath.MathDecimal(userDec));
+            Console.WriteLine("Please enter a number: ");
+            // input from user
+            string userString = Console.ReadLine();
+            // call MathString method returns int
+            Console.WriteLine(getMath.MathString(userString));
+
+            Console.ReadLine();
+        }
+    }
+}

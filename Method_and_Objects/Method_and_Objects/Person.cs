fa26a868c8d3c1335a4153a9ee7a0528230366b5
joelmindset <82834522+joelmindset@users.noreply.github.com>
2021-08-14 10:07:25 -0600

    Method and Objects
    Object initialization and inheritance

---
+++ Method_and_Objects/Method_and_Objects/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_and_Objects
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    
+        public void SayName(string FirstName, string LastName)
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+        }
+    }        
+}
---
+++ Method_and_Objects/Method_and_Objects/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_and_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // object initialization and inheritance
+            Employee EmpPerson = new Employee();
+            // using employee and super persons
+            EmpPerson.FirstName = "Sample";
+            EmpPerson.LastName = "Student";
+            EmpPerson.SayName(EmpPerson.FirstName, EmpPerson.LastName);
+            Console.ReadLine();
+        }
+    }
+}

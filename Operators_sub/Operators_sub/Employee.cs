b99640a16de05e4c1410f3dfa1a6adfb68c6bb6c
joelmindset <82834522+joelmindset@users.noreply.github.com>
2021-08-17 09:17:33 -0600

    added comments

--- Operators_sub/Operators_sub/Employee.cs
+++ Operators_sub/Operators_sub/Employee.cs
@@ -5,17 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Operators_sub
-{
+{   
+    // inherit Person class first and last name create employee class
     public class Employee : Person
     {
+        // instance variables
         public int empID { get; set; }
 
 
-
+        // override == return boolean
         public static bool operator ==(Employee emp1, Employee emp2)
         {
             return emp1.empID == emp2.empID;
         }
+        // override != return boolean
         public static bool operator !=(Employee emp1, Employee emp2)
         {
             return emp1.empID != emp2.empID;

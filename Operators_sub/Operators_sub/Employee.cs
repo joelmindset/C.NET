7adf8bda5d273925d2c4c83644018398edd80192
joelmindset <82834522+joelmindset@users.noreply.github.com>
2021-08-16 19:54:30 -0600

    operators
    overload operators

---
+++ Operators_sub/Operators_sub/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_sub
+{
+    public class Employee : Person
+    {
+        public int empID { get; set; }
+
+
+
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            return emp1.empID == emp2.empID;
+        }
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return emp1.empID != emp2.empID;
+        }
+
+    }
+}
---
+++ Operators_sub/Operators_sub/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_sub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee() { empID = 1, FirstName = "Jesse", LastName = "Johnson" };
+            Employee emp2 = new Employee() { empID = 2, FirstName = "Rob", LastName = "Smith" };
+
+            Console.WriteLine(emp1 == emp2);
+                      
+            Console.ReadLine();
+        }
+    }
+}

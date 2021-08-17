148d0bf249a0e0e061d0e0989bf3e09c0bacdee4
joelmindset <82834522+joelmindset@users.noreply.github.com>
2021-08-17 10:37:58 -0600

    Enums
    use enum for day of week

---
+++ Enums_DayofWeek/Enums_DayofWeek/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums_DayofWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // prompt user 
+            Console.WriteLine("What day of the week is it? Please enter with the first letter capitalized.");
+            // object user input
+            string userDay = Console.ReadLine();
+            
+            DayOfWeek day;
+            // switch for user input of day of week
+            try
+            {   
+                // parse enum for the day
+                day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), userDay);
+                switch (day)
+                {
+                    case DayOfWeek.Sunday:
+                        Console.WriteLine("Sunday it is!");
+                        break;
+                    case DayOfWeek.Monday:
+                        Console.WriteLine("Monday it is!");
+                        break;
+                    case DayOfWeek.Tuesday:
+                        Console.WriteLine("Tuesday it is!");
+                        break;
+                    case DayOfWeek.Wednesday:
+                        Console.WriteLine("Wednesday it is!");
+                        break;
+                    case DayOfWeek.Thursday:
+                        Console.WriteLine("Thursday it is!");
+                        break;
+                    case DayOfWeek.Friday:
+                        Console.WriteLine("Friday it is!");
+                        break;
+                    case DayOfWeek.Saturday:
+                        Console.WriteLine("Saturday it is!");
+                        break;
+                }
+
+            }
+            // if user input wrong return error
+            catch (Exception ex)
+            {
+                Console.WriteLine("sorry please enter a day of the week with the first letter capitalized. example: \"Tuesday\"");
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Enums_DayofWeek/Enums_DayofWeek/WeekDay.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums_DayofWeek
+{
+    public class WeekDay
+    {
+        // instance variable
+        public DayofWeek DayofWeek { get; set; }
+    }
+
+    // enum for week days
+    public enum DayofWeek
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}

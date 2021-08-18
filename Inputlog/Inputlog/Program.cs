9fdc0557cc8d13018176c62888023b2edd28962c
joelmindset <82834522+joelmindset@users.noreply.github.com>
2021-08-18 14:54:03 -0600

    inputLog
    Log an input from a user

---
+++ Inputlog/Inputlog/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Inputlog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\joelm\logs\log.txt", true))
+            {
+                file.WriteLine(num);
+            }
+        }
+    }
+}

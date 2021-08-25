fa4c01a15dd75ccd39d23a369507c4cc36299d0e
joelmindset <82834522+joelmindset@users.noreply.github.com>
2021-08-25 12:38:07 -0600

    newletter

---
+++ NewsletterAppMVC/NewsletterAppMVC/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewsletterAppMVC.Models;
+using NewsletterAppMVC.ViewModels;
+
+namespace NewsletterAppMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (NewsletterEntities db = new NewsletterEntities())
+            {
+                //var signups = db.SignUps.Where(x => x.Removed == null).ToList();
+                var signups = (from c in db.SignUps
+                               where c.Removed == null
+                               select c).ToList();
+                var signupsVms = new List<SignupVm>();
+                foreach (var signup in signups) {
+                    var signupVM = new SignupVm();
+                    signupVM.Id = signup.Id;
+                    signupVM.FirstName = signup.FirstName;
+                    signupVM.LastName = signup.LastName;
+                    signupVM.EmailAddress = signup.EmailAddress;
+                    signupsVms.Add(signupVM);
+                }
+
+                return View(signupsVms);
+            }
+        }
+
+        public ActionResult Unsubscribe(int Id)
+        {
+            using (NewsletterEntities db = new NewsletterEntities()) 
+            {
+                var signup = db.SignUps.Find(Id);
+                signup.Removed = DateTime.Now;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
